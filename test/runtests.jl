22c72dc4f6eb47568cdf1a55b95d07e4a61a9c17
Xavier Nayrac <xavier.nayrac@gmail.com>
2019-03-05 14:49:47 +0100

    Allow to run the tests from a single file

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,12 @@ using .PathOfLight
 
 # FIXME We must be in the project root directory for the following to
 # work.
-for file in readdir("test")
-  if length(file) > 8 && file[end-7:end] == "_test.jl"
-    include(file)
+if length(ARGS) > 0
+  include(ARGS[1] * "_test.jl")
+else
+  for file in readdir("test")
+    if length(file) > 8 && file[end-7:end] == "_test.jl"
+      include(file)
+    end
   end
 end

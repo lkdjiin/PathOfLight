f3d6c1674d68976a893f9e4ec53cd4d7f443bde8
Xavier Nayrac <xavier.nayrac@gmail.com>
2019-03-11 18:57:01 +0100

    Wrap all tests in a testset
    So one can see the overall test count.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,16 @@ include("../src/PathOfLight.jl")
 using Test
 using .PathOfLight
 
-# FIXME We must be in the project root directory for the following to
-# work.
-if length(ARGS) > 0
-  include(ARGS[1] * "_test.jl")
-else
-  for file in readdir("test")
-    if length(file) > 8 && file[end-7:end] == "_test.jl"
-      include(file)
+@testset "PathOfLight Tests" begin
+  # FIXME We must be in the project root directory for the following to
+  # work.
+  if length(ARGS) > 0
+    include(ARGS[1] * "_test.jl")
+  else
+    for file in readdir("test")
+      if length(file) > 8 && file[end-7:end] == "_test.jl"
+        include(file)
+      end
     end
   end
 end

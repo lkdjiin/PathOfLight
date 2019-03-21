39c3bd0416a76f015b8c58da9cbbcb153023f5f5
Xavier Nayrac <xavier.nayrac@gmail.com>
2019-03-21 07:28:05 +0100

    Remove useless exports

--- src/PathOfLight.jl
+++ src/PathOfLight.jl
@@ -5,58 +5,23 @@ import Base: ==, !=, +, -, *, /
 using LinearAlgebra
 using UUIDs
 
-export Noise, PerlinNoise
 include("noise.jl")
 include("perlin.jl")
-
-export Element, Point, Agent, ispoint, isvector, negate, magnitude,
-       normalize, dot, cross, reflect
 include("element.jl")
-
-export Color, rbg
 include("color.jl")
-
-export Canvas, read_pixel, write_pixel!
 include("canvas.jl")
-
-export to_ppm
 include("ppm.jl")
-
-export matrix_compare, identity4, is_invertible, translation, scaling,
-       rotation_x, rotation_y, rotation_z, shearing, view_transform
 include("matrix.jl")
-
-export Ray, location, transform
 include("ray.jl")
-
-export Shape
 include("shape.jl")
-export Sphere
 include("sphere.jl")
-export Plane
 include("plane.jl")
 include("cube.jl")
-
-export Intersection, intersects, intersections, hit, prepare_computations
 include("intersection.jl")
-
-export normal_at
 include("normal.jl")
-
-export PointLight
 include("light.jl")
-
-export pattern_at, StripePattern, GradientPattern, RingPattern, CheckersPattern,
-       RadialGradientPattern, StripeCheckersPattern, BlendedPattern
 include("pattern.jl")
 include("composed_pattern.jl")
-
-export Material, lighting
 include("material.jl")
-
-export World, default_world, intersect_world, shade_hit, color_at, isshadowed
 include("world.jl")
-
-export Camera, ray_for_pixel, render
 include("camera.jl")
-

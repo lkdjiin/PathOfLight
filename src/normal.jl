54002b86cbca6099844b693a78616f614c161c3b
Xavier Nayrac <xavier.nayrac@gmail.com>
2019-03-22 13:46:28 +0100

    Factor normal_at behavior

--- src/normal.jl
+++ src/normal.jl
@@ -1,45 +1,40 @@
-function normal_at(s::Sphere, world_point::Element)
-  object_point = inv(s.transform) * world_point
-  object_normal = object_point - Point(0, 0, 0)
-  world_normal = permutedims(inv(s.transform)) * object_normal
+function normal_at(s::Shape, world_point::Element)
+  local_point = inv(s.transform) * world_point
+  local_normal = NormalH.normal_at(s, local_point)
+  world_normal = permutedims(inv(s.transform)) * local_normal
   normalize(Agent(world_normal))
 end
 
-function normal_at(c::Cylinder, world_point::Element)
-  local_point = inv(c.transform) * world_point
+module NormalH
+  using Main: Element, Agent, Point, Sphere, Cylinder, Plane, Cube
 
-  dist = local_point.x^2 + local_point.z^2
-  if dist < 1 && local_point.y >= c.maximum - epsilon
-    local_normal = Agent(0, 1, 0)
-  elseif dist < 1 && local_point.y <= c.minimum + epsilon
-    local_normal = Agent(0, -1, 0)
-  else
-    local_normal = Agent(local_point.x, 0, local_point.z)
+  function normal_at(shape::Sphere, point::Element)
+    point - Point(0, 0, 0)
   end
 
-  world_normal = permutedims(inv(c.transform)) * local_normal
-  normalize(Agent(world_normal))
-end
-
-function normal_at(p::Plane, world_point::Element)
-  local_point = inv(p.transform) * world_point
-  local_normal = Agent(0, 1, 0)
-  world_normal = permutedims(inv(p.transform)) * local_normal
-  normalize(Agent(world_normal))
-end
-
-function normal_at(c::Cube, world_point::Element)
-  local_point = inv(c.transform) * world_point
-
-  maxc = max(abs(local_point.x), abs(local_point.y), abs(local_point.z))
-  if maxc == abs(local_point.x)
-    local_normal = Agent(local_point.x, 0, 0)
-  elseif maxc == abs(local_point.y)
-    local_normal = Agent(0, local_point.y, 0)
-  else
-    local_normal = Agent(0, 0, local_point.z)
+  function normal_at(shape::Cylinder, point::Element)
+    dist = point.x^2 + point.z^2
+    if dist < 1 && point.y >= shape.maximum - Main.epsilon
+      Agent(0, 1, 0)
+    elseif dist < 1 && point.y <= shape.minimum + Main.epsilon
+      Agent(0, -1, 0)
+    else
+      Agent(point.x, 0, point.z)
+    end
   end
 
-  world_normal = permutedims(inv(c.transform)) * local_normal
-  normalize(Agent(world_normal))
+  function normal_at(shape::Plane, point::Element)
+    Agent(0, 1, 0)
+  end
+
+  function normal_at(shape::Cube, point::Element)
+    maxc = max(abs(point.x), abs(point.y), abs(point.z))
+    if maxc == abs(point.x)
+      Agent(point.x, 0, 0)
+    elseif maxc == abs(point.y)
+      Agent(0, point.y, 0)
+    else
+      Agent(0, 0, point.z)
+    end
+  end
 end

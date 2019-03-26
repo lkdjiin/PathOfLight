be5efcfc5987bede8dc10e28cb75fc1401c837d7
Xavier Nayrac <xavier.nayrac@gmail.com>
2019-03-26 12:48:46 +0100

    Enable/disable shadow for a whole group

--- src/group.jl
+++ src/group.jl
@@ -1,10 +1,14 @@
+# shadow: You can disable the cast of a shadow for the whole group by
+#   setting this field to `:off`. It's `:on` by default like a normal
+#   shape.
 mutable struct Group <: Shape
   id::String
   transform
+  shadow
   childs::Array{Shape}
   parent::Union{Shape, Nothing}
 
-  Group() = new(string(UUIDs.uuid1()), identity4, [], nothing)
+  Group() = new(string(UUIDs.uuid1()), identity4, :on, [], nothing)
 end
 
 function add_child!(g::Group, s::Shape)
